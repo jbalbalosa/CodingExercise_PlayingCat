d783e3e9877f1e55f5ca91f9dcb50748035a1ba3
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2022-11-25 15:12:42 +0900

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Udemy: Java Programming Masterclass");
+        System.out.println("Coding Exercise: Equality Printer");
+        System.out.println("");
+
+
+        System.out.println( isCatPlaying(true, 10));
+        System.out.println( isCatPlaying(false, 36));
+        System.out.println( isCatPlaying(false, 35));
+    }
+
+    public static boolean isCatPlaying(boolean summer, int temperature){
+        if (summer == true && (temperature >=25 && temperature <=45)) {
+            return  true;
+        } else {
+            if (temperature >= 25 && temperature <= 35) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
 }
